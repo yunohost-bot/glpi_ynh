b59bc3df07749a86be43de8849a3e267069f7d67
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-27 12:39:08 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION=8.0
 
-php_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-fileinfo php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysqli php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-xmlrpc php$YNH_PHP_VERSION-bz2 php$YNH_PHP_VERSION-zip"
+php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-fileinfo php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysqli php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-xmlrpc php$YNH_PHP_VERSION-bz2 php$YNH_PHP_VERSION-zip"
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="$php_dependencies"
