87569a04de8eece3b4a8ef110d21043db3177022
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-26 09:41:46 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION=8.0
+#REMOVEME? YNH_PHP_VERSION=8.0
 
-php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-fileinfo php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysqli php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-xmlrpc php$YNH_PHP_VERSION-bz2 php$YNH_PHP_VERSION-zip"
+#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-fileinfo php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-mysqli php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-intl php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-xmlrpc php$YNH_PHP_VERSION-bz2 php$YNH_PHP_VERSION-zip"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
+#REMOVEME? pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
